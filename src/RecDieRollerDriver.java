c38219fb9ad5cf510868edc9fe78ee69dbf5944f
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-08-28 12:34:02 -0500

    Add comments

--- src/RecDieRollerDriver.java
+++ src/RecDieRollerDriver.java
@@ -3,22 +3,29 @@ public class RecDieRollerDriver {
 
 	public static void main(String[] args) {
 
-		
+		// Instantiate RecDieRoller
 		RecDieRoller roller = new RecDieRoller();
-		
+
+		// Create array of integers to store roll counts
 		int[] rollResults = new int[1000];
+
+		// Variables to calculate average roll count
 		double avgRolls, sum = 0;
-		
-		for(int i = 0; i < rollResults.length; i++) {
+
+		// Roll 1000 times, pushing the roll count to rollResults array
+		for (int i = 0; i < rollResults.length; i++) {
 			rollResults[i] = roller.roll(0);
 		}
 
-		for(int roll : rollResults) {
+		// Sum all roll counts in rollResults
+		for (int roll : rollResults) {
 			sum += roll;
 		}
-		
+
+		// Calculate the average roll of rollResults
 		avgRolls = sum / 1000;
-		
+
+		// Print the average number of rolls
 		System.out.println("It took an average of " + avgRolls + " rolls to roll a 2.");
 	}
 
